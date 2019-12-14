9e74d20294941815fac46e74bdb39532462a5f33
bernat <bernatesquirol@gmail.com>
2019-12-14 23:28:47 +0100

    Updates

--- precache-manifest.13f7ebc114609edf3ef02cee121a7430.js
+++ precache-manifest.13f7ebc114609edf3ef02cee121a7430.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee7855cf34dd09edde2fc84fc7b228bd",
+    "revision": "756cbdc07055a3535401b162944b4063",
     "url": "/soccer-story/index.html"
   },
   {
-    "revision": "bb4e5686a1413ddacaab",
-    "url": "/soccer-story/static/css/main.6487e446.chunk.css"
+    "revision": "fc55847537ed9dbf8637",
+    "url": "/soccer-story/static/css/main.1d39ebd3.chunk.css"
   },
   {
-    "revision": "2defc2550bc12b0aab4a",
-    "url": "/soccer-story/static/js/2.e0fa2475.chunk.js"
+    "revision": "1f7dba58d653eae1a288",
+    "url": "/soccer-story/static/js/2.18228ce4.chunk.js"
   },
   {
     "revision": "9fd58da940c5359c971ea5cfe95d4811",
-    "url": "/soccer-story/static/js/2.e0fa2475.chunk.js.LICENSE"
+    "url": "/soccer-story/static/js/2.18228ce4.chunk.js.LICENSE"
   },
   {
-    "revision": "bb4e5686a1413ddacaab",
-    "url": "/soccer-story/static/js/main.7781912a.chunk.js"
+    "revision": "fc55847537ed9dbf8637",
+    "url": "/soccer-story/static/js/main.e5d8b884.chunk.js"
   },
   {
     "revision": "2950d395c17293f7d8f0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/soccer-story/precache-manifest.725b52a703e7345bad51005d209e171d.js"
+  "/soccer-story/precache-manifest.13f7ebc114609edf3ef02cee121a7430.js"
 );
 
 self.addEventListener('message', (event) => {
